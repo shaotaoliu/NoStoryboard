9e62826072c0e998bed315dbf430788dadea4ee4
Liu <bruce.liu@experian.com>
2022-03-22 20:57:22 -0700

    Add NoStoryboard

---
+++ NoStoryboard (AppDelegate)/NoStoryboard/AppDelegate.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // Delete main from app
+    // Delete everything from Info.plist
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
---
+++ NoStoryboard (AppDelegate)/NoStoryboard/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
